237c35d179c0f6795a7b2cac540ae6814ee1cb27
Christian Barbosa <christian.barbosa05222001@gmail.com>
2023-08-21 06:46:02 +0800

    Update logo

---
+++ app.js
@@ -0,0 +1,14 @@
+//Example
+
+
+var nameOfClient = "Christian\" Barbosa\" BSIT\" AI-41\""; //create to allow " to be read as not ending in string//
+
+//declaring Variables
+
+var Age; // the var --- meaning you can change it throughout the program
+let speed = 100; //let --- means that it is declared to a specific functions;
+const limit = 200; //const ---- means you can't change this value if you change it you will encounter error/
+
+
+console.log(nameOfClient);
+console.log(Age);
